952764528711e3e4579e27d240ff6bfaedf6830c
PrabhaDevalapalli <151557898+PrabhaDevalapalli@users.noreply.github.com>
2023-11-21 19:26:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // console.log("document loaded");
-  let usersDetails = localStorage.getItem("users");
+// Function to validate the date of birth
+function validateDateOfBirth() {
+    var dobInput = document.getElementById("dob");
+    var dobValue = new Date(dobInput.value);
+    var currentDate = new Date();
+    var minDate = new Date(currentDate.getFullYear() - 55, currentDate.getMonth(), currentDate.getDate());
+    var maxDate = new Date(currentDate.getFullYear() - 18, currentDate.getMonth(), currentDate.getDate());
 
-  // console.log(usersDetails);
-  if (usersDetails) {
-    // console.log("in if ");
-    let storedData = JSON.parse(usersDetails);
-    // console.log(storedData);
-    users = [...storedData];
-    // console.log(users);
-    let htmlContent = ``;
-    users.forEach(function (userDetails) {
-      htmlContent += `<tr>
-      <td>${userDetails.name}</td>
-      <td>${userDetails.email}</td>
-      <td>${userDetails.password}</td>
-      <td>${userDetails.dob}</td>
-      <td>${userDetails.terms}</td>
-    </tr>`;
+    if (dobValue < minDate || dobValue > maxDate) {
+        alert("Date of birth must be between 18 and 55 years.");
+        dobInput.value = ""; // Clear the invalid date
+    } else {
+        alert("Thank you for providing a valid date of birth.");
+    }
+}
+
+// Function to load saved data from web storage
+function loadSavedData() {
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    var tableBody = document.getElementById("tableBody");
+
+    // Clear table body
+    tableBody.innerHTML = "";
+
+    // Populate table with saved data
+    savedData.forEach(function (data) {
+        var row = tableBody.insertRow();
+        for (var key in data) {
+            var cell = row.insertCell();
+            cell.textContent = data[key];
+        }
     });
-    tableBody.innerHTML = htmlContent;
-  } else {
-    return;
-  }
+}
+
+// Function to handle form submission
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var dob = document.getElementById("dob").value;
+    var acceptTerms = document.getElementById("acceptTerms").checked;
+
+    var formData = { name: name, email: email, password: password, dob: dob, acceptTerms: acceptTerms };
+
+    // Save data to web storage
+    var savedData = JSON.parse(localStorage.getItem("registrationData")) || [];
+    savedData.push(formData);
+    localStorage.setItem("registrationData", JSON.stringify(savedData));
+
+    // Load and display saved data
+    loadSavedData();
+    
+    // Validate date of birth and show an alert
+    validateDateOfBirth();
 });
 
-function showError(message) {
-  errorContainer.textContent = "";
-  errorContainer.textContent = message;
-}
-
-// global variable
-let users = [];
-//  targeting elements
-let errorContainer = document.querySelector(".error-msg");
-let form = document.getElementById("formData");
-let nameElement = document.getElementById("name");
-let emailElement = document.getElementById("email");
-let passwordElement = document.getElementById("password");
-let dobElement = document.getElementById("dob");
-let checkBoxElement = document.getElementById("agree");
-let tableBody = document.getElementById("tableBody");
-let btn = document.getElementById("submit");
-
-//validation functions
-
-function isNameEmpty(name) {
-  return name === "";
-}
-function isEmailEmpty(email) {
-  return email === "";
-}
-
-function isPasswordEmpty(password) {
-  return password == "";
-}
-function isAgeEmpty(age) {
-  return age == "";
-}
-function isInvalidAge(age) {
-  let currentDate = new Date();
-  let userDob = new Date(age);
-  let userAge = currentDate.getFullYear() - userDob.getFullYear();
-  return userAge < 18 || userAge > 55;
-}
-
-//responding to submit event
-
-form.addEventListener("submit", function (event) {
-  console.log("Form submission started");
-
-  event.preventDefault();
-
-  let userName = nameElement.value;
-  let userEmail = emailElement.value;
-  let userPassword = passwordElement.value;
-  let userDob = dobElement.value;
-  let acceptedTerms = checkBoxElement.checked;
-
-  console.log("Form data captured");
-
-  if (isNameEmpty(userName)) {
-    showError("Name Cannot Be Empty, Please Fill That field");
-    return;
-  }
-  if (isEmailEmpty(userEmail)) {
-    showError("Email is Required, Please Fill That Field");
-    return;
-  }
-  if (isPasswordEmpty(userPassword)) {
-    showError("Please Fill The Password");
-    return;
-  }
-  if (isAgeEmpty(userDob)) {
-    showError("Date of Birth is Required");
-    return;
-  }
-  if (isInvalidAge(userDob)) {
-    showError("Your Age Should be Between 18 and 55");
-    return;
-  }
-  // console.log("validations completed");
-  showError("");
-
-  let user = {
-    name: userName,
-    email: userEmail,
-    password: userPassword,
-    dob: userDob,
-    terms: acceptedTerms,
-  };
-  users.push(user);
-  localStorage.setItem("users", JSON.stringify(users));
-
-  location.reload();
-});
+// Load saved data when the page loads
+window.onload = loadSavedData;
